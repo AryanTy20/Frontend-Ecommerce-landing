971d14755d919b444c023572e81b925565c8d44f
AryanTy20 <devty007@gmail.com>
2022-09-21 23:03:44 +0530

    refactor: remove <> </>

--- src/components/PopImg/index.tsx
+++ src/components/PopImg/index.tsx
@@ -15,51 +15,49 @@ const PopImg: React.FC<IProp> = ({ images, closePop, openedImgId }) => {
   const [imgIndex, setImgIndex] = useState<number>(openedImgId);
 
   return (
-    <>
-      <motion.div
-        initial={{
-          opacity: 0,
-        }}
-        animate={{
-          opacity: 1,
-        }}
-        className="pop-img"
-      >
-        <div className="container">
-          <div className="close" onClick={() => closePop((prev) => !prev)}>
-            <IconClose />
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      className="pop-img"
+    >
+      <div className="container">
+        <div className="close" onClick={() => closePop((prev) => !prev)}>
+          <IconClose />
+        </div>
+        <div className="image">
+          <div
+            className="control-left"
+            onClick={() => imgPrev({ images, imgIndex, setImgIndex })}
+          >
+            <IconPrevious />
           </div>
-          <div className="image">
-            <div
-              className="control-left"
-              onClick={() => imgPrev({ images, imgIndex, setImgIndex })}
-            >
-              <IconPrevious />
-            </div>
-            <div className="poster">
-              <img src={images[imgIndex].poster} alt="" />
-            </div>
-            <div
-              className="control-right"
-              onClick={() => imgNext({ images, imgIndex, setImgIndex })}
-            >
-              <IconNext />
-            </div>
-            <div className="thumbnails">
-              {images?.map((val, i) => (
-                <div
-                  className={`box ${imgIndex === i ? "active" : ""}`}
-                  key={i}
-                  onClick={() => setImgIndex(i)}
-                >
-                  <img src={val.thumbnail} alt="" />
-                </div>
-              ))}
-            </div>
+          <div className="poster">
+            <img src={images[imgIndex].poster} alt="" />
+          </div>
+          <div
+            className="control-right"
+            onClick={() => imgNext({ images, imgIndex, setImgIndex })}
+          >
+            <IconNext />
+          </div>
+          <div className="thumbnails">
+            {images?.map((val, i) => (
+              <div
+                className={`box ${imgIndex === i ? "active" : ""}`}
+                key={i}
+                onClick={() => setImgIndex(i)}
+              >
+                <img src={val.thumbnail} alt="" />
+              </div>
+            ))}
           </div>
         </div>
-      </motion.div>
-    </>
+      </div>
+    </motion.div>
   );
 };
 
