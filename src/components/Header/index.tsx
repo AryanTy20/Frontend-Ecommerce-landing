22a68c05267e173b3a72ef30a14d908a3c0b79e0
AryanTy20 <devty007@gmail.com>
2022-09-21 21:56:08 +0530

    refactor: :recycle: add button in place of div

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -63,14 +63,15 @@ const Header: React.FC = () => {
           </nav>
         </div>
         <div className="right">
-          <div
+          <button
+            aria-label="cart"
             className="cart"
             data-shownumber={cartSize > 0 ? "true" : "false"}
             data-value={cartSize}
             onClick={() => setShowCart(!showCart)}
           >
             <IconCart />
-          </div>
+          </button>
           <div className="profile">
             <img src={Avatar} alt="" />
           </div>
@@ -123,7 +124,11 @@ const Cart: React.FC = () => {
         ) : (
           data?.map((item, i) => <CartItem data={item} id={i} key={i} />)
         )}
-        {cartLength > 0 && <button className="checkout">Checkout</button>}
+        {cartLength > 0 && (
+          <button className="checkout" aria-label="checkout">
+            Checkout
+          </button>
+        )}
       </div>
     </motion.div>
   );
@@ -154,7 +159,7 @@ const CartItem: React.FC<listProps> = ({ data, id }) => {
   return (
     <div className="item">
       <div className="thumbnail">
-        <img src={data.img} alt="" />
+        <img src={data.img} alt="item-thumbnail" />
       </div>
       <div className="meta">
         <p>{data?.title}</p>
@@ -163,9 +168,13 @@ const CartItem: React.FC<listProps> = ({ data, id }) => {
           <span> x {data?.quantity} </span> <b>${Total}</b>
         </p>
       </div>
-      <div className="remove" onClick={() => dispatch(remove(id))}>
+      <button
+        aria-label="remove item from cart"
+        className="remove"
+        onClick={() => dispatch(remove(id))}
+      >
         <IconDelete />
-      </div>
+      </button>
     </div>
   );
 };
